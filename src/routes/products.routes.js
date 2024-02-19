ca272a59e401fd00d4a56b12932454c028fda856
LOctavioDev <octaviodevtech@gmail.com>
2024-02-18 22:02:21 -0600

    Fix: La Rutas

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -11,7 +11,6 @@ const router = Router();
 
 router.get("/", getAllProducts);
 router.get("/:bc", getOneProduct);
-router.get("/delete/:bc", getOneDeleteProduct);
 router.post("/", insertProduct);
 router.post("/:bc", updateProduct);
 router.delete("/:bc", deleteProduct);
